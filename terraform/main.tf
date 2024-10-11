10d0b56f87326c4dc4aa9517360394d043a5d7ec
Clement Tan <clementtly@gmail.com>
2024-10-11 10:45:53 +0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,16 @@
 resource "azurerm_resource_group" "this" {
   name = "hello"
 }
+
+resource "azurerm_storage_account" "example" {
+  name                     = "storageaccountname"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+  min_tls_version          = "TLS1_0"
+
+  tags = {
+    environment = "staging"
+  }
+}
